8e37efb68686c82237e12a7a05645335d02da517
Shivaansh Kapoor <wlxmath@WLX-B109-STU26.in.scu.k12.ca.us>
2019-02-01 09:17:06 -0800

    asds

--- 3DShape/src/TriPrism.java
+++ 3DShape/src/TriPrism.java
@@ -1,12 +1,18 @@
 
 public class TriPrism extends Prism{
 	private double base;
-	
+	private double hyp;
+	private double a;
 	public TriPrism(double base, double height, double length) {
 		super(height, length);
+		this.a = base/2;
 		this.base = base;
+		this.hyp = Math.sqrt((a * a) + (height * height));
 	}
-	public getSA() {
-		
+	public double getSA() {
+		return (base * height) + (2 * (hyp * length)) + (base * length);
+	}
+	public double getVolume() {
+		return ((base * height)/2) * length;
 	}
 }
